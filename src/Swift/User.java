9d9a9ad5a2f6e4b3f2fd1ac6b4a56cdcab086944
Eduardo Costa <eduardocosta942z@gmail.com>
2025-08-12 11:39:14 -0300

    src/Swift/User done

---
+++ src/Swift/User.java
@@ -0,0 +1,45 @@
+package Swift;
+
+public abstract class User {
+    // Declare attributes:
+    protected String name;
+    protected int age;
+    protected String cpf;
+
+    // Implement builder method
+    public User(String name, int age, String cpf){
+        this.name = name;
+        this.age = age;
+        this.cpf = cpf;
+    }
+
+    // Implement getters and setters methods:
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Declare behaviour methods
+    public boolean checkAge(){
+        return age >= 18;
+    }
+    public boolean checkCPF(){
+        return cpf.length() == 12;
+    }
+
+    // Implement methods
+    public abstract boolean login();
+    public abstract boolean updateProfile();
+}
